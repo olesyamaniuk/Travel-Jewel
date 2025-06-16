c1792dc6fdb079d01e895b08da0510ff808e7996
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-06-16 14:41:48 +0300

    1

---
+++ js/slick.js
@@ -0,0 +1,15 @@
+$(function () {
+  $(".t-y-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+
+    cssEase: "linear",
+    prevArrow: ".t-y-reviews-list-left",
+    nextArrow: ".t-y-reviews-list-right",
+  });
+});
